66a707691c6f054f45db22d1cf6115c351ff1780
Francesco Pesce <fpesce@MacBook-Pro-2.science.domain>
2022-06-20 10:54:35 +0200

    Update

--- WIP/block_tools.py
+++ WIP/block_tools.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def blocker(array, multi=1):
     dimension = len(array)
     rep = dimension/multi
@@ -15,7 +14,6 @@ def blocker(array, multi=1):
             block_sizes.append(bs)
 
     return dimension, np.array(n_blocks), block_sizes
-
 
 def check(array, multi=1):
     nt = len( blocker(array, multi=multi)[1] )
@@ -62,13 +60,25 @@ def blocking(array, multi=1):
 
     return np.flip( np.array([block_sizes, errs, errs_errs]).T , axis=0  )
 
+def fblocking(cv, w, kbt, multi=1, interval=None):
 
-def optimal_block(stat, method="hline"):
-
-    if method == "hline":
-        c = np.zeros(len(stat))
-        for i,b in enumerate(stat[...,1]):
-            for p in stat:
-                if (b <= p[1]+p[2]) and (b >= p[1]-p[2]):
-                    c[i] += 1
-        return stat[...,0][np.argmax(c)], stat[...,1][np.argmax(c)]
+    N, n_blocks, block_sizes = blocker(cv, multi=multi)
+    u, bins = np.histogram(cv,weights=w,bins=50)
+    u = u/N
+    
+    err = np.zeros(len(block_sizes))
+    err_err = np.zeros(len(block_sizes))
+    for b in range(len(block_sizes)):
+        Nb = n_blocks[b]
+        his = np.zeros(len(bins)-1)
+        for n in range(Nb):
+            start = int( n*block_sizes[b] )
+            end = int( start + block_sizes[b] )
+            hi = np.histogram(cv[start:end], weights=w[start:end], bins=bins)[0] / len(cv[start:end])
+            his += (hi-u)**2
+        e = np.sqrt( his / (Nb*(Nb-1)) )
+        e = kbt*e/u
+        err[b] += e.mean()
+        err_err[b] += err[b] / np.sqrt( 2*(Nb-1) )
+    
+    return np.flip( np.array([block_sizes, err, err_err]).T , axis=0  )
--- WIP/main.py
+++ WIP/main.py
@@ -4,10 +4,23 @@ from block_tools import *
 
 class BlockAnalysis:
 
-    def __init__(self, x, multi=1):
+    def __init__(self, x, multi=1, weights=None, bias=None, T=None, interval=None):
         self.multi = multi
         self.x = check(x, self.multi)
-        self.stat = blocking(self.x, self.multi)
+        self.w = weights
+        self.kbT = 0.008314463*T
+        if (self.w is None) and (bias is not None):
+            bias -= np.max(bias)
+            self.w = np.exp(bias/(self.kbT))
+            self.w = check(x, self.multi)
+
+        if self.w is None:
+            self.stat = blocking(self.x, self.multi)
+            self.av = self.x.mean()
+        else:
+            self.w /= self.w.sum()
+            self.stat = fblocking(self.x, self.w, self.kbT, self.multi)
+
 
     def err(self):
 
@@ -15,7 +28,7 @@ class BlockAnalysis:
                 c=0
                 for i,p in enumerate(stat):
                     if (x <= p[1]+p[2]) and (x >= p[1]-p[2]):
-                        c += (np.pi*p[2]) * np.exp(-0.5*((x-p[1])/p[2])**2)
+                        c += (np.sqrt(2*np.pi)*p[2]) * np.exp(-0.5*((x-p[1])/p[2])**2)
                 return -c
 
         c = np.zeros(len(self.stat))
@@ -23,6 +36,5 @@ class BlockAnalysis:
             lower_bound = b[1]-b[2]
             upper_bound = b[1]+b[2]
             bnds = [(lower_bound, upper_bound)]
-            c[i] -= minimize( fun=find_n_intersect, x0=b[1], args=self.stat[self.stat[...,0] > b[0]], bounds=bnds ).fun    
+            c[i] -= minimize( fun=find_n_intersect, x0=b[1], args=self.stat[self.stat[...,0] > b[0]], bounds=bnds ).fun
         return self.stat[...,0][np.argmax(c)], self.stat[...,1][np.argmax(c)]
-

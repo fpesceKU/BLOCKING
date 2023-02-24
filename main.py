0a1518a618ed13777ae21d24cb44f608fc2a1456
fpesceKU <57720021+fpesceKU@users.noreply.github.com>
2023-02-24 14:04:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+#/*
+# * ----------------------------------------------------------------------------
+# * "THE BEER-WARE LICENSE" (Revision 42):
+# * Francesco Pesce <francesco.pesce@bio.ku.dk> wrote this file.  As long as you retain this notice you
+# * can do whatever you want with this stuff. If we meet some day, and you think
+# * this stuff is worth it, you can buy me a beer in return.
+# * ----------------------------------------------------------------------------
+# */
+
 import numpy as np
 from scipy.optimize import minimize
 from block_tools import *
